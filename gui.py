e5871be365ef87ca088afbec180c58e9a0b88b8a
PakinWasu <pakin.wasu2002@gmail.com>
2024-04-05 14:55:39 +0700

    b

--- gui.py
+++ gui.py
@@ -24,14 +24,11 @@ def sort_nodes_by_distance(graph, source_node):
     sorted_nodes = sorted(distances, key=distances.get)  # เรียงโนดตามระยะทางจากน้อยไปมาก
     return sorted_nodes
 
-
-
 def find_path_edge(path):
     src_dest = []
     for sd_edge in range(len(path)-1):
         closest_path = nx.shortest_path(g, path[sd_edge], path[sd_edge+1], weight='weight')
         src_dest.append(closest_path)
-    
     srcdest_edge = []
     for sub in src_dest :
         for res in sub:
@@ -42,7 +39,6 @@ def find_path_edge(path):
         if srcdest_edge[i] != srcdest_edge[i - 1]:  # ถ้าข้อมูลไม่เท่ากับข้อมูลก่อนหน้า (ไม่ซ้ำ)
             edge.append(srcdest_edge[i])  # เพิ่มข้อมูลเข้าไปใน new_list        
     return edge
-        
 def find_path(slcnode):    
     path = [slcnode[0]]
     keep_u = []
@@ -55,27 +51,19 @@ def find_path(slcnode):
             # print(sort_i[sort_i.index(k)])
             # print(sort_i.index(k))
             # print(sort_i)
-            
             if sort_i.index(k) <= lestes and sort_i.index(k) != 0 and sort_i[sort_i.index(k)] != 'SV02' and sort_i[sort_i.index(k)] != 'SV01' and sort_i[sort_i.index(k)] not in path :
                 lestes = sort_i.index(k)
         # print(lestes)
         path.append(sort_i[lestes])   
-    
-    
     # print(path)
-
     path.insert(len(path),'SV02')
     # print(path)
-    
-
     # # path=['FV02', 'FV02','FV01', 'SV02']    
     short_path = []
     for shot in range(len(path)-1):
         closest_path = nx.shortest_path(g, path[shot], path[shot+1], weight='weight')
         short_path.append(closest_path)
-
     # print(short_path)
-    
     short_path_use = []
     for sublist in short_path:
         for asd in sublist:
@@ -107,31 +95,19 @@ def draw_graph():
     
     print('โนดที่เลือก',selected_nodes)
     pathsh = find_path_edge(find_path_edge(selected_nodes))
-    
-
-    
-    
-    
     selected_edges = []  
-    
     for edge in db.get_distanc():
         for index_slc in range(len(pathsh)-1):
             if (edge[0] in pathsh[index_slc] and edge[1] in pathsh[index_slc+1]):
                 selected_edges.append(edge)
-
     print(selected_edges)
     network.add_nodes_from(selected_nodes)
     network.add_weighted_edges_from(selected_edges)
-    
-
     pos = db.get_pos_node()
-
     fig, ax = plt.subplots(figsize=(16, 7.14))  # Set the size of the graph
     img = mpimg.imread("BG.png")  # Load the background image
     ax.imshow(img, aspect='auto')  # Set the background image of the graph
-
     nx.draw(network, pos, ax=ax, with_labels=True, node_size=250, node_color='black', font_size=6, edge_color='blue', font_color='white')
-
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas_widget = canvas.get_tk_widget()
@@ -140,31 +116,23 @@ def draw_graph():
 
 
 
-
 root = tk.Tk()
 root.title('makro')
-
 # Create a Canvas for the background image
 canvas = tk.Canvas(root, width=1600, height=900, relief='flat')
 canvas.pack()
-
 # Create the graph
 draw_graph()
-
 # Create the Listbox
 listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 listbox.place(x=800, y=720)
-
 # Add items to the Listbox
 items = db.get_productname()
 for item in items:
     listbox.insert(tk.END, item)
-
 # Bind the selection event to the Listbox
 listbox.bind("<<ListboxSelect>>", on_select)
-
 # Create the Label to display selected items
 label = tk.Label(root, text="")
 label.place(x=950, y=830)
-
 root.mainloop()
